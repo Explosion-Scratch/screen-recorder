41dfdccbf39fdf024ad43638de15c63c8c286932
Explosion-Scratch <explosion.scratch@gmail.com>
2022-10-03 23:46:12 +0000

    Deploying to gh-pages from @ Explosion-Scratch/screen-recorder@af7378af5c3f7bd44989cb5815eb97330e106c45 🚀

---
+++ worker.js
@@ -0,0 +1,31 @@
+importScripts(
+  "https://rawcdn.githack.com/bgrins/videoconverter.js/42def8c4136b03cbe55951fbf6fcda214f51059a/build/ffmpeg.js"
+);
+
+function print(text) {
+  postMessage({
+    type: "stdout",
+    data: text,
+  });
+}
+onmessage = function (event) {
+  var module = {
+    files: event.data.files || [],
+    arguments: event.data.arguments || [],
+    print: print,
+    printErr: print,
+  };
+  postMessage({
+    type: "start",
+    data: module.arguments,
+  });
+  var result = ffmpeg_run(module);
+  postMessage({
+    type: "done",
+    data: result,
+  });
+};
+
+postMessage({
+  type: "ready",
+});
